a154cd5b1dffb796bf20275b5301eac813bac95a
Takamasa-Fukase <ultrafukase@gmail.com>
2026-06-14 13:46:25 +0900

    リファクタリング

--- Assets/Scripts/ARController.cs
+++ Assets/Scripts/ARController.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
 
 public class ARController : MonoBehaviour
 {    
@@ -10,14 +8,13 @@ public class ARController : MonoBehaviour
     [SerializeField] GameObject sphere;
     [SerializeField] GameObject target;
     Camera cam;
-    private List<ShotBullet> shotBullets = new();
+    private readonly List<ShotBullet> shotBullets = new();
     private int nextBulletId = 1;
     // 2秒で10メートル移動するための速度を計算（5メートル/秒）
     private float distancePerSecond = 10f / 2f;
-    private List<GameObject> targets = new();
+    private readonly List<GameObject> targets = new();
     private GameObject currentPistolParent;
-    // private Vector3 pistolFPSishPosition = new Vector3(1.651289f, -1.85f, 5.788595f);
-    // private Quaternion pistolFPSishRotaion = Quaternion.Euler(-3.908f, 75.45f, 2.119f);
+
     private GameObject pistol;
     private Vector3 pistolFPSishEulerAngles = new();
 
@@ -44,7 +41,7 @@ public class ARController : MonoBehaviour
         GenerateTargetsToRandomPositions();
 
         currentPistolParent = Instantiate(pistolParent, cam.transform.position, cam.transform.rotation);
-        pistol = currentPistolParent.gameObject.transform.Find("colt1911").gameObject;
+        pistol = currentPistolParent.transform.Find("colt1911").gameObject;
         pistolFPSishEulerAngles = pistol.transform.localEulerAngles;
     }
 
@@ -103,7 +100,7 @@ public class ARController : MonoBehaviour
 
         // 原点0（プレーヤーの初期位置）の超至近距離に的を集中させずに、
         // なるべくばらけさせることができる様に制御する
-        if (randomX < -(closeRangeBorder) || randomX > closeRangeBorder || randomY < -(closeRangeBorder) || randomY > closeRangeBorder)
+        if (randomX < -closeRangeBorder || randomX > closeRangeBorder || randomY < -closeRangeBorder || randomY > closeRangeBorder)
         {
             // XとYのどちらかが原点0から0.5より離れている場合は、
             // 原点0に近い値（-0.5 ~ 0.5）も含めてユーザーの近くも遠くも両方含めた範囲のZのランダム値を渡す
@@ -117,14 +114,14 @@ public class ARController : MonoBehaviour
             randomZ = Random.value < 0.5f ? randomZNegativeAndExceptCloseRange : randomZPositiveAndExceptCloseRange;
         }
 
-        Vector3 randomPosition = new Vector3(randomX, randomY, randomZ);
+        Vector3 randomPosition = new(randomX, randomY, randomZ);
         return randomPosition;
     }
 
     private void SendMessageToAndroid(UnityToAndroidMessage message)
     {
         print($"SendMessageToAndroid message: {message}");
-        AndroidJavaObject unityToAndroidMessenger = new AndroidJavaObject("com.takamasafukase.ar_gunman_android.UnityToAndroidMessenger");
+        AndroidJavaObject unityToAndroidMessenger = new("com.takamasafukase.ar_gunman_android.UnityToAndroidMessenger");
         // 構造体からJSON文字列に変換
         string jsonStringMessage = JsonUtility.ToJson(message);
 
@@ -175,7 +172,7 @@ public class ARController : MonoBehaviour
         GameObject bulletInstance = Instantiate(sphere, cam.transform.position, Quaternion.identity);
 
         // 発射済みの球を管理する為の構造体を生成
-        ShotBullet newBullet = new ShotBullet(
+        ShotBullet newBullet = new(
             id: nextBulletId++,
             targetDirection: cam.transform.forward,
             bullet: bulletInstance
@@ -206,19 +203,18 @@ public class ARController : MonoBehaviour
             // 的に弾が当たったことをAndroid側に通知
             SendMessageToAndroid(message: message);
         }
-        GameObject? hitTargetObject = targets.Find(target => target == targetObject);
+        GameObject hitTargetObject = targets.Find(target => target == targetObject);
         if (hitTargetObject != null)
         {
             print("該当の的のオブジェクトを配列から削除します");
-            targets.Remove((GameObject)hitTargetObject);
+            targets.Remove(hitTargetObject);
         }
     }
 
     private void KeepPistolPositionAndAngleToFPSIsh()
     {
         // ピストルをラップしている空の親オブジェクトに対する操作
-        currentPistolParent.transform.position = cam.transform.position;
-        currentPistolParent.transform.rotation = cam.transform.rotation;
+        currentPistolParent.transform.SetPositionAndRotation(cam.transform.position, cam.transform.rotation);
 
         // ピストル自体のオブジェクトに対する操作（ユーザー操作によってはズレる場合があるため補正する）
         pistol.transform.localEulerAngles = pistolFPSishEulerAngles;
